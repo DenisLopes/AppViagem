bcfd902957381fe9690c805d90e006da4570e77d
Denis <denisds@ciandt.com>
2021-06-30 21:54:30 -0300

    Implementação no layout com * UIStackwiew *UICollectionView *UICollectionViewCell

---
+++ app-viagem/app-viagem/Pacotes/View/PacoteViagemCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PacoteViagemCollectionViewCell.swift
+//  app-viagem
+//
+//  Created by Denis Carlos Lopes da Silva on 30/06/21.
+//
+
+import UIKit
+
+class PacoteViagemCollectionViewCell: UICollectionViewCell {
+    
+}
