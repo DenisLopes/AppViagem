ab2aa8dc00891fdb6b8af414ca93c30668f03024
Denis <denisds@ciandt.com>
2021-07-01 21:20:15 -0300

    Finalizando a tela de pacotes

--- app-viagem/app-viagem/Pacotes/Controller/PacotesViagensViewController.swift
+++ app-viagem/app-viagem/Pacotes/Controller/PacotesViagensViewController.swift
@@ -7,25 +7,67 @@
 
 import UIKit
 
-class PacotesViagensViewController: UIViewController, UICollectionViewDataSource {
+class PacotesViagensViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
     @IBOutlet weak var colecaoPacotesViagens: UICollectionView!
+    @IBOutlet weak var pesquisarViagens: UISearchBar!
+    @IBOutlet weak var labelContadorPacotes: UILabel!
+    
+    let listaComTodasViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    var listaViagens:Array<Viagem> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        listaViagens = listaComTodasViagens
         colecaoPacotesViagens.dataSource = self
+        colecaoPacotesViagens.delegate = self
+        pesquisarViagens.delegate = self
+        self.labelContadorPacotes.text = self.atualizaContadorlabel()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return listaViagens.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       
         let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
-        celulaPacote.backgroundColor = UIColor.blue
+        let viagemAtual = listaViagens[indexPath.item]
+        
+        celulaPacote.labelTitulo.text = viagemAtual.titulo
+        celulaPacote.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
+        celulaPacote.labelPreco.text = "R$ \(viagemAtual.preco)"
+        celulaPacote.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
+        
+        celulaPacote.layer.borderWidth = 0.5
+        celulaPacote.layer.borderColor = UIColor(red: 85.0/255, green: 85.0/255, blue: 85.0/255, alpha: 1).cgColor
+        celulaPacote.layer.cornerRadius = 8
         
         return celulaPacote
     }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let larguraCelula = collectionView.bounds.width/2
+        return CGSize(width: larguraCelula-15, height: 160)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        listaViagens = listaComTodasViagens
+        if searchText != "" {
+            let filtroListaViagem = NSPredicate(format: "titulo contains %@", searchText)
+            let listaFiltrada:Array<Viagem> = (listaViagens as NSArray).filtered(using: filtroListaViagem) as! Array
+            listaViagens = listaFiltrada
+        }
+        
+        self.labelContadorPacotes.text = self.atualizaContadorlabel()
+        colecaoPacotesViagens.reloadData()
+    }
+    
+    func atualizaContadorlabel() -> String {
+        return listaViagens.count == 1 ? "1 pacote encontrado" : "\(listaViagens.count) pacotes encontrados"
+    }
+    
     
 
 
--- app-viagem/app-viagem/Pacotes/View/PacoteViagemCollectionViewCell.swift
+++ app-viagem/app-viagem/Pacotes/View/PacoteViagemCollectionViewCell.swift
@@ -9,4 +9,9 @@ import UIKit
 
 class PacoteViagemCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var imagemViagem: UIImageView!
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+    
 }
