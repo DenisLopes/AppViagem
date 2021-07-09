d54f8608ea96a81a15b5bf7a269cdd9669845c08
Denis <denisds@ciandt.com>
2021-07-09 12:57:16 -0300

    Finalizando Scroll View

--- app-viagem/app-viagem/DetalhesViagens/Controller/DetalhesViagensViewController.swift
+++ app-viagem/app-viagem/DetalhesViagens/Controller/DetalhesViagensViewController.swift
@@ -14,10 +14,17 @@ class DetalhesViagensViewController: UIViewController {
     @IBOutlet weak var labelDescricaoPacoteViagem: UILabel!
     @IBOutlet weak var labelDataViagemPacoteViagem: UILabel!
     @IBOutlet weak var labelPrecoPacoteViagem: UILabel!
+    @IBOutlet weak var scrollPrincipal: UIScrollView!
+    
+    
+    
     
     var pacoteSelecionado:PacoteViagem? = nil
 
     override func viewDidLoad() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(aumentarScroll(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
         super.viewDidLoad()
         if let pacote = pacoteSelecionado {
             self.imagemPacoteViagem.image = UIImage(named: pacote.viagem.caminhoDaImagem)
@@ -27,6 +34,10 @@ class DetalhesViagensViewController: UIViewController {
             self.labelPrecoPacoteViagem.text = pacote.viagem.preco
         }
 
+    }
+    
+    @objc func aumentarScroll(notification: Notification){
+        self.scrollPrincipal.contentSize = CGSize(width: self.scrollPrincipal.frame.width, height: self.scrollPrincipal.frame.height + 320)
     }
     
     @IBAction func botaoVolatar(_ sender: UIButton) {
